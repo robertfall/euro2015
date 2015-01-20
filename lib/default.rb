598930080307796bae78cfcfd3add64c7a40840d
Robert Fall <rob@robertfall.com>
2015-01-20 09:59:00 +0100

    Start splitting previous posts into countries

--- lib/default.rb
+++ lib/default.rb
@@ -1,5 +1,6 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+require 'active_support/core_ext/string/inflections'
 
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Tagging
