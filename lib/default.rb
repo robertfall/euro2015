a0ae9b4cc1725e8e63b7d38b176aecba416ca328
Robert Fall <rob@robertfall.com>
2015-01-22 16:37:08 +0200

    Add photo gallery

--- lib/default.rb
+++ lib/default.rb
@@ -4,3 +4,12 @@ require 'active_support/core_ext/string/inflections'
 
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::LinkTo
+
+def images_for(country)
+  items.select {|i| i.identifier =~ /images\/#{country}/}
+end
+
+def countries
+  articles.map { |a| a[:country] }.uniq
+end
