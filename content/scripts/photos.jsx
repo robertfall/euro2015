c22208b760571d0d211ec647c4a095f571861d0b
Robert Fall <rob@robertfall.com>
2015-02-10 23:16:09 +0100

    Basic image gallery

---
+++ content/scripts/photos.jsx
@@ -0,0 +1,86 @@
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+var If = React.createClass({
+    render: function() {
+        if (this.props.test) {
+            return this.props.children;
+        }
+        else {
+            return false;
+        }
+    }
+});
+
+var Thumb = React.createClass({
+  style: function() {
+    return {
+      backgroundImage: 'url(/images/' + this.props.country + '/thumbs/' + this.props.photo + ')'
+    };
+  },
+  handleClick: function() {
+    this.props.onClick(this.props.country, this.props.photo);
+  },
+  render: function() {
+    return (
+      <a className='picture' style={this.style()} onClick={this.handleClick}/>
+    );
+  }
+});
+
+var Preview = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <img src={this.props.url} />
+        <a className='download-link' href={this.props.url}>Right click here and click save as to download this photo</a>
+      </div>
+    );
+  }
+});
+
+var Gallery = React.createClass({
+  getInitialState: function() {
+    return {}
+
+  },
+  handleClick: function(country, photo) {
+    console.log('clicked')
+    var photoUrl = "/images/" + country + "/" + photo;
+    this.setState({selectedPhoto: photoUrl});
+  },
+
+  render: function() {
+    var headerClass = 'gallery-header ' + this.props.country.class;
+    return (
+      <div>
+        <div className='gallery'>
+          <div className={headerClass}>
+            {this.props.country.name}
+          </div>
+          <If test={this.state.selectedPhoto}>
+              <Preview url={this.state.selectedPhoto} />
+          </If>
+          {this.props.country.thumbs.map(function(thumb) {
+            return <Thumb country={this.props.country.code} photo={thumb} onClick={this.handleClick} />;
+          }, this)}
+        </div>
+      </div>
+    );
+  }
+});
+
+var Photos = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.countries.map(function(country) {
+          return <Gallery country={country} />;
+        })}
+      </div>
+    );
+  }
+})
+
--- lib/default.rb
+++ lib/default.rb
@@ -1,6 +1,7 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 require 'active_support/core_ext/string/inflections'
+require 'json'
 
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Tagging
@@ -8,7 +9,19 @@ include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::XMLSitemap
 
 def images_for(country)
-  items.select {|i| i.identifier =~ /images\/#{country}/}
+  items.select {|i| i.identifier =~ /images\/#{country}\/\d\d/}
+end
+
+def thumbs_for(country)
+  items.select {|i| i.identifier =~ /images\/#{country}\/thumbs\/\d\d/}
+end
+
+def thumbnail_json
+  collection = []
+  countries.each do |c|
+    collection << { name: c, code:c.parameterize.underscore, class: c.parameterize, thumbs: thumbs_for(c.parameterize.underscore).map { |t| File.basename(t[:filename])}}
+  end
+  collection.to_json
 end
 
 def countries
